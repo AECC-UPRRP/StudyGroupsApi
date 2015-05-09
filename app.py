2940c33627bea51a06ca30fbf1f7925450dd375d
Julio de la Cruz <jjdl_cn@hotmail.com>
2015-05-09 14:08:54 -0400

    Using Postgres

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
+
 from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:////tmp/test.db")
 db = SQLAlchemy(app)
 
 
